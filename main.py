583147c777d059e352934b0da7efb2980c27a4b7
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-05-14 10:23:51 -0700

    quiz game completed

---
+++ main.py
@@ -0,0 +1,18 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    new_question = Question(
+        question["question"], question["correct_answer"]
+    )  # object of class Question
+    question_bank.append(new_question)
+# print(question_bank)
+
+quiz = QuizBrain(question_bank)  # initialize
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You're completed the quiz")
+print(f"Your final score was: {quiz.score}/{len.question_bank()}")
